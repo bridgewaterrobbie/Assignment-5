14650207625f7e074f67d43236fd88a5b9aff01a
Sarah Miller <sarahcmiller@ufl.edu>
2016-02-01 00:11:31 -0500

    Implemented incomplete functions in ListingsController
    Implemented $scope.update to update listings and $scope.remove to remove
    listings.

--- client/app/controllers/listings.client.controller.js
+++ client/app/controllers/listings.client.controller.js
@@ -79,6 +79,17 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         successfully finished, navigate back to the 'listing.list' state using $state.go(). If an error 
         occurs, pass it to $scope.error. 
        */
+       if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+
+        return false;
+      }
+      Listings.update(listing)
+              .then(function(response) {
+                $state.go('listing.list', { successMessage: 'Listing successfully updated!' });
+              }, function(error) {
+                $scope.error = 'Unable to update listing!\n' + error;
+              });
     };
 
     $scope.remove = function() {
@@ -86,6 +97,12 @@ angular.module('listings').controller('ListingsController', ['$scope', '$locatio
         Implement the remove function. If the removal is successful, navigate back to 'listing.list'. Otherwise, 
         display the error. 
        */
+       Listings.remove(listing)
+              .then(function(response) {
+                $state.go('listing.list', { successMessage: 'Listing successfully removed!' });
+              }, function(error) {
+                $scope.error = 'Unable to remove listing!\n' + error;
+              });
     };
 
     /* Bind the success message to the scope if it exists as part of the current state */
